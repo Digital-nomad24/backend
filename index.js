fc0455a893d474582b3a740272dd44ed2d41e74f
Nakshatra Vashisth <nakshvashisth@gmail.com>
2024-03-14 01:26:29 +0530

    two modifications

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ if(process.env.NODE_ENV==="production")
 {
     const __dirname=path.resolve();
 
-    app.use(express.static(path.join(__dirname, '/frontend/dist')));
+    app.use(express.static(path.join(__dirname, 'public/index.html')));
     app.use('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'frontend',"dist", 'index.html'));
+    res.sendFile(path.join(__dirname,"public", 'index.html'));
 });
 }
 else
