413473a4107ab7b0038a0b46e33c2c9545a81529
Nakshatra Vashisth <nakshvashisth@gmail.com>
2024-03-13 16:36:25 +0530

    sfsa

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use('/api/v1/user',userRoutes)
 app.use('/api/v1/account',bankRoutes)
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
-// Define a catch-all route that serves index.html for all other routes
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+// // Define a catch-all route that serves index.html for all other routes
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// });
 
 app.listen('3000',()=>{
     console.log("The port 3000 is live")
