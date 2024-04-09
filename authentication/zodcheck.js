73d3fc19f2cd7046a73d18b03f4520c33215955a
Nakshatra Vashisth <nakshvashisth@gmail.com>
2024-04-09 22:28:16 +0530

    modified url

--- authentication/zodcheck.js
+++ authentication/zodcheck.js
@@ -1,14 +1,14 @@
 const { z } = require('zod');
 
-export const userZod = z.object({
+const userZod = z.object({
     username: z.string().min(1),
     firstName: z.string().min(1),
     lastName: z.string().min(1),
     password: z.string().min(1),
     Email: z.string().email().min(1)
 });
-export const signinzod=z.object({
+const signinzod=z.object({
     email:z.string().email().min(1),
     password: z.string().min(1)
 })
-
+module.exports={userZod,signinzod}
