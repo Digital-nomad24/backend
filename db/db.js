efdb81c475d4d21757f4625078d82bfc172783bc
Nakshatra Vashisth <nakshvashisth@gmail.com>
2024-07-02 16:50:33 +0530

    asd213

--- db/db.js
+++ db/db.js
@@ -23,15 +23,4 @@ const bankSchema={
     balance:Number
 }
 const Account=mongoose.model('Account',bankSchema)
-userSchema.post('findOneAndDelete',async function(doc){
-    // console.log("HO GYA DELETE")
-    if(doc)
-    {
-        await Account.deleteMany({
-            _id:{
-                $in: doc.reviews
-            }
-        })
-    }
-})
 module.exports={User,Account};
