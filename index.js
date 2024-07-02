3092109860223926a756f837090141545aa1040c
Nakshatra Vashisth <nakshvashisth@gmail.com>
2024-07-02 13:44:26 +0530

    url

--- index.js
+++ index.js
@@ -6,14 +6,13 @@ const app=express();
 const userRoutes=require('./routes/user')
 const bankRoutes=require('./routes/bank')
 const path=require('path')
-// const corsOptions = {
-//     origin: 'https://example.com', // Replace with your allowed origin
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//     allowedHeaders: ['Content-Type', 'Authorization'],
-//     credentials: true
-//   };
-  
-  app.use(cors());
+const corsOptions = {
+    origin: 'https://frontend-seven-gamma-86.vercel.app', // Your front-end URL
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true // if you need to include cookies or HTTP authentication
+  };
+  app.use(cors(corsOptions))
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use('/api/v1/user',userRoutes)
